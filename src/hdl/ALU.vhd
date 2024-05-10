019ba209ad2247a57aa66d319f45dde3b22fe092
Groseb <C26Georgina.Buchanan@afacademy.ad.edu>
2024-05-10 16:58:51 -0600

    LAB5

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -24,9 +24,11 @@
 --| ALU OPCODES:
 --|
 --|     ADD     000
---|
---|
---|
+--|     SUB     001
+--|     AND     010
+--|     LSH     101
+--|     RSH     100
+--|     OR      011
 --|
 --+----------------------------------------------------------------------------
 library ieee;
@@ -36,16 +38,37 @@ library ieee;
 
 entity ALU is
 -- TODO
+    generic(N: integer := 8);
+    Port (i_A : in STD_LOGIC_VECTOR (N-1 downto 0);
+          i_B : in STD_LOGIC_VECTOR (N-1 downto 0);
+          o_flag : out STD_LOGIC_VECTOR (2 downto 0);
+          o_ALU : out STD_LOGIC_VECTOR (N-1 downto 0);
+          op : in STD_LOGIC_VECTOR (2 downto 0));
 end ALU;
 
 architecture behavioral of ALU is 
   
 	-- declare components and signals
-
+signal w_B      : STD_LOGIC_VECTOR (N-1 downto 0);
+signal w_ALUout : STD_LOGIC_VECTOR (N-1 downto 0);
+signal w_cin    : STD_LOGIC_VECTOR (0 downto 0);
+signal w_cout   : STD_LOGIC;
+signal w_add    : STD_LOGIC_VECTOR (N downto 0);
   
 begin
 	-- PORT MAPS ----------------------------------------
-
+    w_cin <= op(0 downto 0);
+    
+    w_B <= not i_B when op = "001" else i_B;
+    w_add(N-1 downto 0) <= std_logic_vector(unsigned(i_A) + unsigned(w_B) + unsigned(w_cin));
+    
+   -- w_shift <= std_logic_vector(shift_left(unsigned(i_A), to_integer(unsigned(i_B(2 downto 0))))) when op(0) = '0';
+   
+   o_ALU <= w_add(N-1 downto 0) when op(2 downto 1) = "00";
+   
+   o_flag(0) <= w_add(N);
+   o_flag(1) <= '1' when w_ALUout = "00000000" else '0';
+   o_flag(2) <= w_ALUout(N-1);
 	
 	
 	-- CONCURRENT STATEMENTS ----------------------------
--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -19,6 +19,9 @@
 --|    sm_<state machine type>  = state machine type definition
 --|    s_<signal name>          = state name
 --|
+--|
+--| Documentation Statement: C3C McClung suggested I use a state
+--| machine and he taught me how to do most of Task A.
 --+----------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
@@ -27,20 +30,151 @@ library ieee;
 
 entity top_basys3 is
 -- TODO
+port (
+         clk   : in std_logic;
+         sw    : in std_logic_vector(15 downto 0);
+         btnU  : in std_logic;
+         btnL  : in std_logic;
+         btnR  : in std_logic;
+         btnC  : in std_logic;
+         
+         led : out std_logic_vector(15 downto 0);
+         
+         seg : out std_logic_vector(6 downto 0);
+         
+         an  : out std_logic_vector(3 downto 0)
+         
+      );
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
-
-  
+    component sevenSegDecoder is
+         port( i_D : in std_logic_vector (3 downto 0);
+               o_S : out std_logic_vector (6 downto 0));
+               end component sevenSegDecoder;
+               
+	component TDM4 is
+	     generic (constant k_WIDTH : natural  := 4);
+	     Port ( i_clk    : in std_logic;
+	            i_reset  : in std_logic;
+	            i_D3     : in std_logic_vector (k_WIDTH - 1 downto 0);
+	            i_D2     : in std_logic_vector (k_WIDTH - 1 downto 0);
+	            i_D1     : in std_logic_vector (k_WIDTH - 1 downto 0);
+	            i_D0     : in std_logic_vector (k_WIDTH - 1 downto 0);
+	            o_data   : out std_logic_vector (k_WIDTH - 1 downto 0);
+	            o_sel    : out std_logic_vector (3 downto 0)
+                );
+          end component TDM4;
+   
+    component clock_divider is
+       generic (constant k_DIV : natural := 2 );
+       
+         port ( i_clk   : in std_logic;
+                i_reset : in std_logic;
+                o_clk   : out std_logic
+         );
+      end component clock_divider;
+      
+    component ALU is
+    generic (N: integer := 0);
+    Port ( i_A : in std_logic_vector (N-1 downto 0);
+           i_B : in std_logic_vector (N-1 downto 0);
+           o_flag : out std_logic_vector (2 downto 0);
+           o_ALU : out std_logic_vector (N-1 downto 0);
+           op : in std_logic_vector (2 downto 0)
+           );
+     end component ALU;
+      
+    component controller_fsm is
+       Port ( i_input : in std_logic_vector(7 downto 0);
+              i_adv   : in std_logic;
+              i_reset : in std_logic;
+              o_S     : out std_logic_vector(3 downto 0);
+              o_A     : out std_logic_vector(7 downto 0);
+              o_B     : out std_logic_vector(7 downto 0)
+              );
+     end component controller_fsm;
+     
+   signal w_A : std_logic_vector(7 downto 0);
+   signal w_B : std_logic_vector(7 downto 0);
+   signal w_cycle : std_logic_vector(3 downto 0);
+   signal w_display : std_logic_vector(7 downto 0);
+   
+   signal w_clk : std_logic;
+   
+--   signal w_negative  : std_logic;
+--   signal w_hundresds : std_logic_vector(3 downto 0);
+--   signal w_tens      : std_logic_vector(3 downto 0);
+--   signal w_ones      : std_logic_vector(3 downto 0);
+signal w_flag : std_logic_vector(2 downto 0);
+   signal w_data : std_logic_vector(3 downto 0);
+   signal w_sel  : std_logic_vector(3 downto 0);
+   
+   signal w_result : std_logic_vector(7 downto 0);
+   signal w_bin    : std_logic_vector(7 downto 0);
+   signal w_ALU    : std_logic_vector(7 downto 0);
+   signal w_op     : std_logic_vector(2 downto 0);
+   
+   signal w_7SD_EN_n : std_logic;
+   
+   signal w_tdmre : std_logic_vector(0 downto 0);
+   
 begin
 	-- PORT MAPS ----------------------------------------
-
-	
-	
+ALU_inst : ALU
+       port map (
+       i_A => w_A,
+       i_B => w_B,
+       o_flag => led(15 downto 13),
+       o_ALU => w_ALU,
+       op => sw(15 downto 13)
+       );
+       
+TDM4_inst : TDM4
+        port map ( i_clk => w_clk,
+                   i_reset => btnL,
+                   i_D3 => w_display(7 downto 4),
+                   i_D2 => w_display(3 downto 0),
+                   i_D1 => w_display(3 downto 0),
+                   i_D0 => w_display(7 downto 4),
+                   o_data => w_data,
+                   o_sel => w_sel
+                   );
+ 
+sevenSegDecoder_inst : sevenSegDecoder
+       port map ( i_D => w_data,
+                  o_S => seg
+                  );
+clkdiv_inst : clock_divider
+       generic map (k_DIV => 100000)
+       port map ( i_clk => clk,
+                  i_reset => btnL,
+                  o_clk => w_clk
+                  );
+                  
+controller_fsm_inst : controller_fsm
+       port map ( i_input => sw(7 downto 0),
+                  i_adv => btnC,
+                  i_reset => btnR,
+                  o_S => w_cycle,
+                  o_A => w_A,
+                  o_B => w_B
+                  );    
+                  
+w_display <= w_ALU;
+w_7SD_EN_n <= '1';
+an(3) <= w_7SD_EN_n;
+an(2) <= w_7SD_EN_n;              
+an(1) <= '1' when w_cycle="1000" or w_sel="1011" or w_sel="1101" else '0';	
+an(0) <= '1' when w_cycle="1000" or w_sel="0111" or w_sel="1110" else '0';	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
-	
-	
+
+w_display <= w_A when w_cycle="0001" else
+w_B when w_cycle="0010" else
+w_ALU when w_cycle="0100" else "00000000";
+
+led(12 downto 4) <= "000000000";
+led(3 downto 0) <= w_cycle;	
 end top_basys3_arch;
